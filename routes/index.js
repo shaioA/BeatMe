b7623287b639d3f7970f4f4436f867e197a04f7e
Shai <shaialkabetz@gmail.com>
2016-01-16 19:35:25 +0200

    Adding main.html

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('__dirname:',__dirname);
+  //console.log('__dirname:',__dirname);
   //res.render('index', { title: 'Express' });
   //res.sendfile('/views/main.html')
   res.sendfile('main.html', {root: 'c:\\Users\\Shai\\WebstormProjects\\BeatMe\\public\\views' })
