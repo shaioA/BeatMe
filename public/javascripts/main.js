92b82366cdd1f00c00c7dad27d940c9fc399f827
maitabe <martushb@gmail.com>
2016-01-24 09:39:54 +0200

    first version of the main pages and conection to the server

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,8 +2,51 @@
  * Created by Shai on 1/16/2016.
  */
 
-var socket = io.connect();
-socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
+
+
+$(document).ready(function(){
+    //show login page when loading application
+    $('#loginPage').show();
+
+    //connect to the server
+    var socket = io.connect();
+
+    // socket listener
+    socket.on('login123_response',function(data){
+        console.log(data);
+
+        if(data){
+            // get in to the app
+            $('#loginPage').hide();
+            $('#iconsPage').show();
+            $('header').css('display', 'block');
+        }
+    });
+
+    //listener game type responds
+    socket.on('gametype_response', function(game) {
+    // getting obj : title, text(arr), icon name
+
+        // insert data to dom
+
+        // get in to the app
+        //$('#loginPage').hide();
+        //$('#gamesPage').show();
+
+
+
+    });
+
+    //click on login button display games page
+   $('.loginButton').click(function() {
+       socket.emit('login123',{user:'shai',pwd:'robinzon'});
+       });
+
+   //go to the game page
+   $('.box > ul > li img').click(function(e) {
+       var game = $(this).attr('data-game');
+       console.log('sending to server...');
+       socket.emit('gametype', {typeGame: game })
+   });
+
 });
