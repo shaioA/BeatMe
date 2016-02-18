e2b583a5444275f1ffec3beaf0d88692923a97ba
shaioa <shaialkabetz@gmail.com>
2016-02-18 20:52:53 +0200

    Add logic game calculate()

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   //console.log('__dirname:',__dirname);
   //res.render('index', { title: 'Express' });
   //res.sendfile('/views/main.html')
-  res.sendFile('main.html', {root: 'c:\\Users\\Martha\\WebstormProjects\\BeatMe\\public\\views' });
+  res.sendFile('main.html', {root: 'c:\\Users\\Sport-Zone\\wsProjects\\BeatMe\\public\\views' });
  //   res.sendFile('main.html', {root: '/webapps/BeatMe/public/views' });
   //res.send('Hello World!');
 });
